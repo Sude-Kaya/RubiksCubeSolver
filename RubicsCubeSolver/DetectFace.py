413f8c284fac389724b9f14fb0cf6d32070737e2
Sude-Kaya <sude20240@gmail.com>
2026-05-05 18:44:58 +0300

    Update DetectFace.py

--- RubicsCubeSolver/DetectFace.py
+++ RubicsCubeSolver/DetectFace.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import Image
 import cv2 as cv
 
 colors = {
@@ -7,7 +6,7 @@ colors = {
 "red" : [0,0,255],
 "blue" : [255,100,0],
 "white" : [255,255,255], 
-"green" : [10,255,0],
+"green" : [0,255,0],
 "orange" : [0,100,255],
 }
 
@@ -22,12 +21,15 @@ def get_limits(color):
     hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
    
     hue = int(hsvC[0][0][0])
-    lower_limit=max(hue - 10,0),30,50
+    lower_limit=max(hue - 10,0),60,60
     upper_limit=min(hue + 10,179),255,255
 
     if color == [255,255,255]:
         lower_limit = 0, 0, 100
         upper_limit= 179, 50, 255
+    elif color == [0,255,0]:
+        lower_limit=max(hue - 10,0),30,60
+        upper_limit=min(hue + 10,179),255,255
 
     lower_limit = np.array(lower_limit, dtype = np.uint8)
     upper_limit = np.array(upper_limit, dtype=np.uint8)
@@ -35,30 +37,27 @@ def get_limits(color):
     return lower_limit, upper_limit
 
 
-
 def detectCellColor(hsv_frame, cell_coords):
     y1, y2 = cell_coords[0]
     x1, x2 = cell_coords[1]
 
     roi = hsv_frame[y1+25:y2-25, x1+25:x2-25]
+    roi = roi[::4, ::4]
+    votes = {}
 
-    avg_color = np.mean(roi, axis=(0,1)) 
+    for pixel in roi.reshape(-1, 3):
+        h, s, v = pixel
+        for name, bgr in colors.items():
+            l, u = get_limits(bgr)
 
-    avg_hue = avg_color[0]
-    avg_saturation = avg_color[1]
-    avg_value = avg_color[2]
+            if (l[0] <= h <= u[0] and
+                l[1] <= s <= u[1] and
+                l[2] <= v <= u[2]):
 
-    for name, bgr in colors.items():
-        l, u = get_limits(bgr)
+                votes[name] = votes.get(name, 0) + 1
+                break
 
-        if (l[0] <= avg_hue <= u[0] and
-            l[1] <= avg_saturation <= u[1] and
-            l[2] <= avg_value <= u[2]):
-            return name
+    if not votes:
+        return "unknown"
 
-    return "unknown"
-
-
-    
-
-        
+    return max(votes, key=votes.get)  
