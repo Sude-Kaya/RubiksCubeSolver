dc7684f1ff480dbf922c02e24d42bf0fe1af4011
Sude-Kaya <sude20240@gmail.com>
2026-05-10 17:12:47 +0300

    Update Main.py

--- RubicsCubeSolver/Main.py
+++ RubicsCubeSolver/Main.py
@@ -6,23 +6,25 @@ from Solver import *
 
 capture = cv.VideoCapture(0)
 
-face_indices = {"front" : [2, 0] , "left" :[4, 2] , "right" : [1, 3] , "up" : [0, 1] , "down" :[3, 4], "back" : [5,5]}
-faces = ["front", "left", "right", "up", "down", "back"]
+face_indices = {"front" : [2, 0] , "left" :[4, 2] , "right" : [1, 3] , "top" : [0, 1] , "bottom" :[3, 4], "back" : [5,5]}
+faces = ["front", "left", "right", "top", "bottom", "back"]
 current_face_idx = 0
 cube_state = np.full((6, 9), "unknown", dtype='<U10')
 scan_complete = False
 phase = "SCANNING"
-
+solution = None
 fig, ax = setup_screen()
+cv.namedWindow("Rubik's Cube Solver", cv.WINDOW_NORMAL)
+cv.resizeWindow("Rubik's Cube Solver", 1800, 900)
 
 def showGrid(face):
-    cv.putText(frame, f"Scan the {face} face", (190,50), cv.FONT_HERSHEY_COMPLEX, 0.8, (0,255,0), 2)
-    cv.rectangle(frame, (100,100), (400,400), (0,255,0), thickness=5)
-    cv.line(frame, (100,200), (400,200), (0,255,0), thickness=5)
-    cv.line(frame, (100,300), (400,300), (0,255,0), thickness=5)
-    cv.line(frame, (200,100), (200,400), (0,255,0), thickness=5)
-    cv.line(frame, (300,100), (300,400), (0,255,0), thickness=5)
-    cv.putText(frame, "Press 'c' to confirm", (190,450), cv.FONT_HERSHEY_COMPLEX, 0.8, (0,255,0), 2)
+    cv.putText(blank, f"Current step: scan the {face} face", (400,50), cv.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 2)
+    cv.rectangle(frame, (100,100), (400,400), (255,255,255), thickness=3)
+    cv.line(frame, (100,200), (400,200), (255,255,255), thickness=3)
+    cv.line(frame, (100,300), (400,300), (255,255,255), thickness=3)
+    cv.line(frame, (200,100), (200,400), (255,255,255), thickness=3)
+    cv.line(frame, (300,100), (300,400), (255,255,255), thickness=3)
+    cv.putText(blank, "Press 'c' to confirm", (500,600), cv.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 2)
 
 def scanFace():
     current_face = np.full(9, "unknown", dtype='<U10')
@@ -34,25 +36,47 @@ def scanFace():
         y1, y2 = cell[0]
         x1, x2 = cell[1]
 
-        cx = (x1 + x2) // 2 - 30
+        cx = (x1 + x2) // 2 - 35
         cy = (y1 + y2) // 2 - 30
 
         cv.putText(frame, cell_color, (cx, cy),
-                   cv.FONT_HERSHEY_COMPLEX, 0.8, (0,255,0), 2)
+                   cv.FONT_HERSHEY_COMPLEX, 0.7, (255,255,255), thickness=2)
 
     return current_face
 
+def render_Cube_Net(blank):
+    fig.canvas.draw()
+    img = np.array(fig.canvas.renderer.buffer_rgba())
+    img = cv.resize(img, (500, 400))
+
+    overlay = img[:, :, :3]
+    overlay = cv.cvtColor(overlay, cv.COLOR_RGB2BGR)
+    alpha = img[:, :, 3] / 255.0
+
+    y1, y2 = 120, 520
+    x1, x2 = 680, 1180
+
+    region = blank[y1:y2, x1:x2]
+
+    for c in range(3):
+        region[:, :, c] = (
+            alpha * overlay[:, :, c] +
+            (1 - alpha) * region[:, :, c]
+        )
+    blank[y1:y2, x1:x2] = region
+
 while True:
+    blank = np.zeros((630,1200,3), dtype='uint8')
     isTrue, frame = capture.read()
     hsv_img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     key = cv.waitKey(1) & 0xFF
-    
     if phase == "SCANNING":
         face = faces[current_face_idx]
 
         showGrid(face)
 
         current_face = scanFace() 
+        
 
         if key == ord('c'):
             target_idx = face_indices[face][0]
@@ -61,21 +85,12 @@ while True:
             formatted_face = [color[0].upper() if color != "unknown" else "unknown" for color in current_face]
             
             draw_face(ax, formatted_face,face_indices[face][1])
-            
-            print(f"{face} Recorded: {formatted_face}")
+           
             current_face_idx += 1
 
-            print(cube_state)
-            print(f"{face} saved:", current_face)
-
             if current_face_idx >= len(faces):
-                print("DONE SCANNING")
                 phase = "SOLVING"
-                
-    elif phase == "SOLVING":
-        cv.putText(frame, f"Solving...", (200,50), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), 2)
-
-        solution = solve_from_colors(
+                solution = solve_from_colors(
           cube_state[0],
           cube_state[1],
           cube_state[2],
@@ -83,13 +98,16 @@ while True:
           cube_state[4],
           cube_state[5]
     )
-
+                
+    elif phase == "SOLVING":
+        cv.putText(blank, f"Solving...", (530,50), cv.FONT_HERSHEY_COMPLEX, 1.0, (255,255,255), 2)
         print("Solution:", solution)
 
-    cv.imshow("Rubik's Cube Solver", frame)
+    blank[80:560, 20:660] = frame
+    render_Cube_Net(blank)
+    cv.imshow("Rubik's Cube Solver", blank)
     if key == ord('d'):
         break
 
 capture.release()
 cv.destroyAllWindows()
-plt.ioff()
